b5f98be3d045737be8d27f7632b9d9e7b2cd7394
Victor Magalhães <victorhfmag@gmail.com>
2020-06-30 17:35:59 -0300

    Change sites

--- _data/sites.js
+++ _data/sites.js
@@ -1,35 +1,11 @@
 module.exports = {
-	netlify: {
-		description: "Netlify web sites",
-		options: {
-			freshChrome: "site",
-		},
-		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			// "https://www.netlify.com/pricing/", // Having Lighthouse issues with LCP on this page
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
-		]
-	},
 	ssg: {
-		description: "Static Site Generator web sites",
+		description: "Meu site",
 		options: {
 			freshChrome: "site"
 		},
 		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			// "https://vuepress.vuejs.org/", // kept timing out in lighthouse
-			"https://gridsome.org/",
+			"https://victormagalhaes.codes/",
 		]
 	}
 };
